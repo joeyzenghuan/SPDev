0bb5d04f75ae68a6a3aae9115c53284352c8044f
joeyzenghuan <joeyzenghuan@outlook.com>
2020-12-11 17:25:58 +0800

    update

--- pnp/connect-pnponline.ps1
+++ pnp/connect-pnponline.ps1
@@ -4,3 +4,4 @@ $cred = New-Object -TypeName System.Management.Automation.PSCredential -argument
 
 Connect-PnPOnline -Url https://m365x502029.sharepoint.com/sites/teamsite2 -Credential $Cred
 
+Connect-PnPOnline -Url https://m365x502029.sharepoint.com/sites/teamsite2 -UseWebLogin
